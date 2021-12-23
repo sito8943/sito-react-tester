3e578001d792f17c6b3704b0eb6368a5ee55f45a
Sito Nimbus <40501794+SitoNumbis@users.noreply.github.com>
2021-12-23 15:27:21 -0800

    algunos comentarios

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import contenedor from "./img/contenedor.png";
 
 function App() {
   // para este ejemplo 74 es el 100%  de un contenedor
+  // 74 vendría siendo la altura de la capa que marca lo que no está lleno en el contenedor
+  // debajo de esta capa está la capa naranja que indica lo que está lleno
+  // las partes del barco, son imágenes con extensión png con las mismas dimensiones h: 160 y w: 118
+  // si se va a mover el barco se tiene que cambiar el margin-top que tiene las clases css carga y no-carga
   const [contenedores, setContenedores] = useState([74, 20, 39, 37]); // arreglo con estados con los números que representan la carga
   return (
     <div className="App">
