e25f5959a2f9f081a299804ba1ff3b5e53b5a7d8
Sito Nimbus <40501794+SitoNumbis@users.noreply.github.com>
2021-12-23 15:15:10 -0800

    stio-react-teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+import cargas from "./img/cargas.png";
+import delantera from "./img/delantera.png";
+import trasera from "./img/trasera.png";
+import contenedor from "./img/contenedor.png";
 
 function App() {
+  // para este ejemplo 74 es el 100%  de un contenedor
+  const [contenedores, setContenedores] = useState([74, 20, 39, 37]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={trasera} alt="trasera" className="trasera" />
+      {contenedores.map((d, i) => {
+        return (
+          <div className="contenedor-div">
+            <img src={cargas} alt="carga" className="carga" />
+            <div style={{ height: d }} className="no-carga"></div>
+            <img src={contenedor} alt="contenedor" className="contenedor" />
+          </div>
+        );
+      })}
+      <img src={delantera} alt="delantera" className="delantera" />
     </div>
   );
 }
