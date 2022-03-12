965a9e47aa05af20e85c9f59603bafd2f00ed12a
SitoNumbis <sito8943@gmail.com>
2022-03-11 21:45:17 -0500

    added buttons components

---
+++ src/components/MemeShare/Button/IconButton/Icons/icons.jsx
@@ -0,0 +1,5 @@
+import { BsArrowRight, BsFillRecordFillIconName } from "react-icons/bs";
+
+export const RightArrowIcon = () => <BsArrowRight />;
+
+export const DotIcon = () => <BsFillRecordFillIconName />;
