99441256fc32569a6b9016ab9d361089cf7b1c55
SitoNumbis <sito8943@gmail.com>
2022-03-11 23:39:38 -0500

    working on time line

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import "./App.css";
 import React from "react";
-
-// components
-import MemeShare, {
-  Title,
-  Container,
-  Paragraph,
-} from "./components/MemeShare/MemeShare";
-import AppleDots from "./components/MemeShare/AppleDots/AppleDots";
-import {
-  JavaIcon,
-  JavascriptIcon,
-  SwiftIcon,
-  PhpIcon,
-  KotlinIcon,
-  TypescriptIcon,
-  CPlusPlusIcon,
-  DartIcon,
-  GoIcon,
-} from "./components/MemeShare/Icons/icons";
+import Timeline from "./components/Timeline/Timeline";
 
 function App() {
   return (
     <div className="App">
-      <MemeShare>
-        <Container>
-          <AppleDots />
-          <Container className="ms-sub-container">
-            <Title variant="h3">Ways to declare a constant</Title>
-            <Container className="flex" style={{ marginTop: 25 }}>
-              <PhpIcon color={"aliceblue span"} />
-              <Title variant="h5" style={{ marginLeft: 15 }}>
-                Php
-              </Title>
-            </Container>
-            <Paragraph className="ms-code">
-              <span className="ms-keyword">definee</span>(
-              <span className="ms-string">"var name"</span>,{" "}
-              <span className="ms-string">"some value"</span>);
-            </Paragraph>
-          </Container>
-        </Container>
-      </MemeShare>
+      <Timeline />
     </div>
   );
 }
